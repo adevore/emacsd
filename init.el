1dc1e1ef504c0fb95ba84da53c2cca494c4c382f
Aaron DeVore <aaron.devore@gmail.com>
2014-06-20 16:04:33 -0700

    Add groovy-mode

--- init.el
+++ init.el
@@ -5,6 +5,9 @@
 (add-to-list 'load-path "~/.emacs.d/evil-numbers")
 (require 'evil-numbers)
 
+(add-to-list 'load-path "~/.emacs.d/Emacs-Groovy-Mode")
+(require 'groovy-mode)
+
 (define-key evil-normal-state-map (kbd "C-c +") 'evil-numbers/inc-at-pt)
 (define-key evil-normal-state-map (kbd "C-c -") 'evil-numbers/dec-at-pt)
 
